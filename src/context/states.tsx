1265de9f7e29e339b17cb2d8faccb89c6876d5bd
CherishShed <c.shed2000@gmail.com>
2023-10-25 01:26:16 +0100

    fixed packages

--- src/context/states.tsx
+++ src/context/states.tsx
@@ -6,23 +6,24 @@ type storetype = {
 };
 type modalType = {
   show: boolean;
-  status:string;
-  showModal: (status:string) => void;
+  status: string;
+  showModal: (status: string) => void;
   hideModal: () => void;
 };
 export const modalStore = create<modalType>()((set) => ({
   show: false,
-  status:"",
-  showModal: (status) => set(() => ({ show: true, status: status })),
+  status: "",
+  showModal: (status: string) => set(() => ({ show: true, status: status })),
   hideModal: () => set(() => ({ show: false, status: "" })),
 }));
 export const myStore = create<storetype>()((set) => ({
-  tasks: [
-  ],
+  tasks: [],
   addTask: (title: string, status: string, id: string) =>
-    set((store) => ({ tasks: [...store.tasks, { id, title, status }] })),
+    set((store: storetype) => ({
+      tasks: [...store.tasks, { id, title, status }],
+    })),
   removeTask: (id: string) => {
-    set((store) => ({
+    set((store: storetype) => ({
       tasks: store.tasks.filter((task) => id !== task.id),
     }));
   },
